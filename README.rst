89024a0f56ef7ebf4a738c0269036f6c2bb6faff
Yue Wu <yuewu@outlook.com>
2016-02-12 23:30:01 +0800

    add group info

--- README.rst
+++ README.rst
@@ -11,6 +11,10 @@ This is a simple command line parser for C++.
 - Only one header file
 - Automatic type check
 
+Note
+-----
+This version is modified by Yue Wu (yuewu@outlook.com). The author add 'group' information so that the options can be organized by groups.
+
 Sample
 ------
 
@@ -36,10 +40,11 @@ This is an example of simple usage.
     // 2nd argument is short name (no short name if '\0' specified)
     // 3rd argument is description
     // 4th argument is mandatory (optional. default is false)
-    // 5th argument is default value  (optional. it used when mandatory is false)
+    // 5th argument is group (optional, default is "")
+    // 6th argument is default value  (optional. it used when mandatory is false)
     a.add<string>("host", 'h', "host name", true, "");
     
-    // 6th argument is extra constraint.
+    // 7th argument is extra constraint.
     // Here, port number must be 1 to 65535 by cmdline::range().
     a.add<int>("port", 'p', "port number", false, 80, cmdline::range(1, 65535));
     
--- test2.cpp
+++ test2.cpp
@@ -30,32 +30,34 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <iostream>
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   cmdline::parser a;
-  a.add<string>("host", 'h', "host name", true, "");
-  a.add<int>("port", 'p', "port number", false, 80, cmdline::range(1, 65535));
-  a.add<string>("type", 't', "protocol type", false, "http", cmdline::oneof<string>("http", "https", "ssh", "ftp"));
-  a.add("help", 0, "print this message");
+  a.add<string>("host", 'h', "host name", true, "", "");
+  a.add<int>("port", 'p', "port number", false, "", 80,
+             cmdline::range(1, 65535));
+  a.add<string>("type", 't', "protocol type", false, "", "http",
+                cmdline::oneof<string>("http", "https", "ssh", "ftp"));
+  a.add("help", 0, "print this message", "");
   a.footer("filename ...");
   a.set_program_name("test");
 
-  bool ok=a.parse(argc, argv);
+  bool ok = a.parse(argc, argv);
 
-  if (argc==1 || a.exist("help")){
-    cerr<<a.usage();
-    return 0;
-  }
-  
-  if (!ok){
-    cerr<<a.error()<<endl<<a.usage();
+  if (argc == 1 || a.exist("help")) {
+    cerr << a.usage();
     return 0;
   }
 
-  cout<<a.get<string>("host")<<":"<<a.get<int>("port")<<endl;
+  if (!ok) {
+    cerr << a.error() << endl
+         << a.usage();
+    return 0;
+  }
 
-  for (size_t i=0; i<a.rest().size(); i++)
-    cout<<"- "<<a.rest()[i]<<endl;
+  cout << a.get<string>("host") << ":" << a.get<int>("port") << endl;
+
+  for (size_t i = 0; i < a.rest().size(); i++)
+    cout << "- " << a.rest()[i] << endl;
 
   return 0;
 }
